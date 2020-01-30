94081acb01bc349027419240ab1e4ef62784493e
Kevin McGrevey <niv3kmcg@gmail.com>
2020-01-29 19:36:41 -0700

    Begin setup for shelter information

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,5 +1,11 @@
 class SheltersController <ApplicationController
+
   def index
     @shelters = Shelter.all
+    require "pry"; binding.pry
+  end
+
+  def show
+    @shelter = Shelter.find(params[:id])
   end
 end
