6b683b1502463ef77347d3002c6c25688093de04
Kevin McGrevey <niv3kmcg@gmail.com>
2020-02-02 20:22:38 -0700

    Consolidation commit of various stages

--- spec/features/shelters/user_can_create_shelters_spec.rb
+++ spec/features/shelters/user_can_create_shelters_spec.rb
@@ -3,11 +3,6 @@ require 'rails_helper'
 RSpec.describe "Create Shelters", type: :feature do
   describe "As a visitor" do
     it 'I can create a shelter' do
-      # shelter_1 = Shelter.create(name: "Mike's Shelter",
-      #                            address: "1331 17th Street",
-      #                            city: "Denver",
-      #                            state: "CO",
-      #                            zip: "80202")
 
       visit '/shelters'
 
@@ -20,14 +15,11 @@ RSpec.describe "Create Shelters", type: :feature do
       fill_in 'City', with: 'Kalamazoo'
       fill_in 'State', with: 'MI'
       fill_in 'Zip', with: '12345'
+
       click_on 'Create Shelter'
 
       expect(current_path).to eq('/shelters')
       expect(page).to have_content('Bozo')
-      # expect(page).to have_content('123 Main')
-      # expect(page).to have_content('Kalamazoo')
-      # expect(page).to have_content('MI')
-      # expect(page).to have_content('12345')
     end
   end
 end
